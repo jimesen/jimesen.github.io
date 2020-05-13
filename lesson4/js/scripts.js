9293801ffbf01d91691158ff52c301b884444295
jimesen <jemesenj@microsoft.com>
2020-05-12 19:37:02 -0600

    updates

--- lesson4/js/scripts.js
+++ lesson4/js/scripts.js
@@ -1,3 +1,7 @@
 function toggleMenu () {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
+
+const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
+document.getElementById("currentdate").textcontent = new Date().toLocaleDateString ('en-US', options);
+
