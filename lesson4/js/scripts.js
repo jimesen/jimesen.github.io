61f12cc4ed4cd8101a648bd5704d6ff2df940b3b
jimesen <jemesenj@microsoft.com>
2020-05-15 22:00:28 -0600

    updated

--- lesson4/js/scripts.js
+++ lesson4/js/scripts.js
@@ -2,6 +2,12 @@ function toggleMenu () {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
 
-const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
-document.getElementById("currentdate").textcontent = new Date().toLocaleDateString ('en-US', options);
+const date = new Date();
+ const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+ const monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+ const dayName = days[date.getDay()];
+ const monthName = monthNames[date.getMonth()];
+ const year = date.getFullYear();
+ document.getElementById("today").innerHTML = dayName + ', ' + date.getDate() + ' ' + monthName  + ' ' + year;
+ document.getElementById("year").innerHTML = year;
 
