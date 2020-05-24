f59ac03e621760a8061e1dcc337661948e6daaf2
jimesen <jemesenj@microsoft.com>
2020-05-23 18:31:30 -0600

    Lesson5Created

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -1,7 +1,9 @@
+// Menu Hamburguer
 function toggleMenu () {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
 
+// Date on the Footer
 const date = new Date();
  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
  const monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
@@ -11,3 +13,12 @@ const date = new Date();
  document.getElementById("today").innerHTML = dayName + ', ' + date.getDate() + ' ' + monthName  + ' ' + year;
  document.getElementById("year").innerHTML = year;
 
+// Pancake Banner
+var d = new Date();
+const banner = document.getElementById("banner");
+if (d.getDay() == 5) {
+    pancakebanner.setAttribute("style", "display: block");
+}
+else {
+    pancakebanner.setAttribute("style", "display: none"); 
+}
