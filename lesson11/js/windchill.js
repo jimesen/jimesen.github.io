4e5f45740660fc407b9c6c7b9f3c6f9d0ef234b3
jimesen <jemesenj@microsoft.com>
2020-07-03 19:40:38 -0600

    updated

--- lesson11/js/windchill.js
+++ lesson11/js/windchill.js
@@ -1,9 +1,19 @@
-var h = document.getElementById('h').innerHTML;
-var w = document.getElementById('w').innerHTML;
-let wc = 35.74+(0.6215*h)-(35.75*(w**0.16))+(0.4275*h*(w**.16));
-if (h <= 50 && w > 3) {
-   wc = Math.round(wc);
-} else {
-   wc = "NA";
-}
-document.getElementById('wC').innerHTML = wc;
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=5fbe37b45777a8e16856c8c4c241f6e7";
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        document.getElementById('current').innerHTML = jsObject.weather[0].description;
+        document.getElementById('cTemp').textContent = jsObject.main.temp;
+        document.getElementById('high').textContent = jsObject.main.temp_max;
+        document.getElementById('humidity').textContent = jsObject.main.humidity;
+        document.getElementById('wS').textContent = jsObject.wind.speed;
+        var w = parseFloat(document.getElementById("cTemp").innerHTML);
+        var s = parseFloat(document.getElementById("wS").innerHTML);
+        var wc = 35.74+(0.6215*w)-35.75*Math.pow(s,0.16)+0.4275*w*Math.pow(s,0.16);
+        if (w < 51 && s > 3) {
+            document.getElementById("wC").innerHTML = Math.round(wc);
+        }
+        else {
+            document.getElementById("wC").innerHTML = "N/A";
+        }
+    });
